6ee131e078578d59d42d16cc3b28e5dcbe6842b1
VABIII <tbengtson82@gmail.com>
2022-01-28 23:32:31 -0600

    created new tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,14 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
+    .createTable('clients', (users) => {
+      users.increments('client_id')
+      users.string('client_name', 200).notNullable()
       users.string('password', 200).notNullable()
+      users.string('role', 200).notNullable()
       users.timestamps(false, true)
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('clients')
 }
