d80aa6aae57cea300f38d80870a6d87678bec22f
VABIII <tbengtson82@gmail.com>
2022-01-30 00:30:56 -0600

    added restricted middleware to classes route

--- api/classes/classes-middleware.js
+++ api/classes/classes-middleware.js
@@ -1,4 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../configs');
 
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+    if(!token) {
+        next({status: 403, message: `token required`})
+    }
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if(err) {
+            next({status: 401, message: 'token invalid'});
+        } else {
+            req.decoded = decoded
+            next()
+        }
+    })
+};
 
 
 
