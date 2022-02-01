5b2bcc91cd992e1ed1d12ae86b266b4f8d1a3607
VABIII <tbengtson82@gmail.com>
2022-02-01 15:42:57 -0600

    trying again

--- api/data/seeds/02-instructors.js
+++ api/data/seeds/02-instructors.js
@@ -4,12 +4,12 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('instructors').insert([
         {instructor_name: 'Remy LeBeau', role: 'instructor', password: '1234'},
-        {instructor_id: 2 ,instructor_name: 'James Howlette', role: 'instructor', password: '1234'},
-        {instructor_id: 3 ,instructor_name: 'Alison Blair', role: 'instructor', password: '1234'},
-        {instructor_id: 4 ,instructor_name: 'Alfred Yankovic', role: 'instructor', password: '1234'},
-        {instructor_id: 5,instructor_name: 'Betsy Braddock', role: 'instructor', password: '1234'},
-        {instructor_id: 6 ,instructor_name: 'Piotr Rasputin', role: 'instructor', password: '1234'},
-        {instructor_id: 7 ,instructor_name: 'Kurt Wagner', role: 'instructor', password: '1234'},
+        {instructor_name: 'James Howlette', role: 'instructor', password: '1234'},
+        {instructor_name: 'Alison Blair', role: 'instructor', password: '1234'},
+        {instructor_name: 'Alfred Yankovic', role: 'instructor', password: '1234'},
+        {instructor_name: 'Betsy Braddock', role: 'instructor', password: '1234'},
+        {instructor_name: 'Piotr Rasputin', role: 'instructor', password: '1234'},
+        {instructor_name: 'Kurt Wagner', role: 'instructor', password: '1234'},
       ]);
     });
 };
